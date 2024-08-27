1002b505e2b1de5ccc39c37bbbb17c170969f156
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2024-08-27 00:34:44 -0300

    ci: error de prueba corregido para el ci 1.0

--- app/src/main/java/com/gg/loginexample/ui/HomeScreen.kt
+++ app/src/main/java/com/gg/loginexample/ui/HomeScreen.kt
@@ -1,16 +1,21 @@
 package com.gg.loginexample.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.gg.loginexample.domain.model.Post
+import com.gg.loginexample.fake.FakeModels
+import com.gg.loginexample.ui.theme.LoginExampleTheme
 
 @Composable
 fun HomeScreen(posts: List<Post>) {
@@ -36,19 +41,19 @@ fun HomeScreen(posts: List<Post>) {
     }
 }
 
-//@PreviewLightDark
-////@PreviewFontScale
-////@PreviewScreenSizes
-//@Composable
-//private fun HomeScreenPreview() {
-//    LoginExampleTheme {
-//        Surface(
-//            modifier = Modifier.fillMaxSize(),
-//            color = MaterialTheme.colorScheme.background
-//        ) {
-//            HomeScreen(
-//                posts = FakeModels.fakePosts
-//            )
-//        }
-//    }
-//}
+@PreviewLightDark
+//@PreviewFontScale
+//@PreviewScreenSizes
+@Composable
+private fun HomeScreenPreview() {
+    LoginExampleTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            HomeScreen(
+                posts = FakeModels.fakePosts
+            )
+        }
+    }
+}
